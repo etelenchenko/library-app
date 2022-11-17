ea073a36a300a22ee69d482b9dfd0448f7fdd96c
eugene.telenchenko <etelenchenko@gmail.com>
2022-11-17 15:05:39 +0300

    added Navbar component

--- library-app-frontend/src/App.tsx
+++ library-app-frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>123</h1>
-    </div>
-  );
+    return (
+        <Navbar/>
+    );
 }
 
 export default App;
